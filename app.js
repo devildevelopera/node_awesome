2ee7787fac6f5a88966273d994593d872f49b88e
cleverjack <example@test.com>
2020-02-07 08:39:21 -0400

    socket update

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ const bodyParser = require('body-parser');
 const mongoose = require('./config/database.js');
 app.use(cors());
 
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+const port = process.env.PORT || 3005
+let server = app.listen(port, function(){
+  console.log('listening on port :', port);
+});
+
+var io = require('socket.io')(server);
 
 io.on('connection', function(client) {
   var online = Object.keys(io.engine.clients);
@@ -30,18 +34,13 @@ app.use(bodyParser.urlencoded({
   }));
 
 const postsRoute = require('./routes/posts');
-const usersRoute = require('./routes/users')
+const usersRoute = require('./routes/users');
 
 app.use('/uploads', express.static('uploads'));
 
 app.use('/posts', postsRoute);
-app.use('/users', usersRoute)
+app.use('/users', usersRoute);
 
 app.get('/', (req, res) => {
     res.send('We are on home');
-});
-
-const port = process.env.PORT || 3005
-http.listen(port, function(){
-  console.log('listening on port :', port);
 });
